dc83c5f713484e334cad3c71b84742c9a017463a
itemhsu <itemhsu@gmail.com>
2020-06-26 09:40:09 +0000

    use diff figure and margin

--- fftLPRect.py
+++ fftLPRect.py
@@ -10,6 +10,9 @@ import math
 from PIL import Image
 import PIL
 import cv2
+isBias=False
+isPlot=False
+
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
@@ -29,9 +32,14 @@ def rectScale(im):
     nim = cv2.resize(im, (myWidth*2, myHeight*2), interpolation=cv2.INTER_AREA)
     return nim
 
-def estCorrect(orgImg, cutoffF=0.8):
-    pix_color = np.array(orgImg)
+def estCorrect(orgImg, cutoffF=0.8, margin=0.1):
+    w=orgImg.shape[1]
+    crop_img = orgImg[:, int(margin*w):int(w-margin*w)]
+    pix_color = np.array(crop_img)
+    full_pix_color = np.array(orgImg)
     img = rgb2gray(pix_color)
+    img=abs(img[0:-1,:]-img[1:,:])
+    
     f = np.fft.fft2(img)
     fshift = np.fft.fftshift(f)
     magnitude_spectrum = 20*np.log(np.abs(fshift))
@@ -43,30 +51,39 @@ def estCorrect(orgImg, cutoffF=0.8):
     size=min(img.shape)
     polar_img = cv2.warpPolar(magnitude_spectrum, (int(size/2), 200), (img.shape[1]/2,img.shape[0]/2), 
                                   size*margin*0.5, cv2.WARP_POLAR_LINEAR)
-    
-    polar_img_lowF=polar_img[:,0:int(cutoffF*polar_img.shape[1])]
+   
+    polar_img_lowF=polar_img[:,int(0.2*polar_img.shape[1]):int(cutoffF*polar_img.shape[1])]
     
     polar_sum_200=np.sum(polar_img_lowF,axis=1)
     polar_sum=polar_sum_200[0:100]+polar_sum_200[100:200]
-    polar_sum[50]=min(polar_sum) #matthew  do not count center line
+    #polar_sum[50]=min(polar_sum) #matthew  do not count center line
+    #print(statistics.stdev(polar_sum[25:75]))
     maxIndex=np.argmax(polar_sum[25:75])+25
     offsetDegree=(maxIndex-50)/100*3.14
     aEst=np.sin(offsetDegree)
-    correctImg=imgWrapA(pix_color,aEst)
+    #correctImg=imgWrapA(pix_color,aEst)
+    correctImg=imgWrapA(full_pix_color,aEst)
+    #full_pix_color
+    #polar_sum[25:75]=min(polar_sum) #matthew  do not count center line
+    #maxIndex2=np.argmax(polar_sum)
+    #print("maxIndex2={}".format(maxIndex2))
+
     return correctImg
 
-def estCorrect2D(orgImg, cutoffF=0.8):
-    hCorrectedImg=estCorrect(orgImg, cutoffF)
-    hCorrectedImg90=np.rot90(hCorrectedImg)
-    vCorrectedImg=estCorrect(hCorrectedImg90, cutoffF)
+def estCorrect2D(orgImg, cutoffF=0.8, margin=0.1):
+    hCorrectedImg=estCorrect(orgImg, cutoffF, margin)
+    hCorrectedImg90=np.rot90(hCorrectedImg, )
+    vCorrectedImg=estCorrect(hCorrectedImg90, cutoffF, margin)
     vCorrectedImg270=np.rot90(vCorrectedImg,  k=3)
+
+    
     return hCorrectedImg, vCorrectedImg270
 
 
-def estCorrect2D_scale(strFpath, cutoffF=0.8):
+def estCorrect2D_scale(strFpath, cutoffF=0.8, margin=0.1):
     orgImg = cv2.imread(strFpath)
     pix_color = np.array(orgImg)
-    hCorrectedImg, vCorrectedImg270=estCorrect2D(pix_color, cutoffF)
+    hCorrectedImg, vCorrectedImg270=estCorrect2D(pix_color, cutoffF, margin)
     imWrap_t_wrap_t_scaled=rectScale(vCorrectedImg270)
     return hCorrectedImg, vCorrectedImg270, imWrap_t_wrap_t_scaled
     
@@ -77,14 +94,19 @@ if __name__ == '__main__':
     imWrap_t_wrap_t_s=[]
     imWrap_t_wrap_t_scaled_s=[]
     cutoffF=0.8
-    for root,unkown,fNames in os.walk('img3'):
-        for f in fNames:
-            strFpath="img3/{}".format(f)
-            hCorrectedImg, vCorrectedImg270, imWrap_t_wrap_t_scaled=estCorrect2D_scale(strFpath, cutoffF)
-            imWrap_s.append(hCorrectedImg)
-            imWrap_t_wrap_t_s.append(vCorrectedImg270)
-            imWrap_t_wrap_t_scaled_s.append(imWrap_t_wrap_t_scaled)
-    
+    margin=0.1
+    dirs={'img','img3', 'img4'}
+    for fDir in dirs:
+        for root,unkown,fNames in os.walk(fDir):
+            for f in fNames:
+                strFpath="{}/{}".format(fDir,f)
+                hCorrectedImg, vCorrectedImg270, imWrap_t_wrap_t_scaled=estCorrect2D_scale(strFpath, cutoffF, margin)
+                imWrap_s.append(hCorrectedImg)
+                imWrap_t_wrap_t_s.append(vCorrectedImg270)
+                imWrap_t_wrap_t_scaled_s.append(imWrap_t_wrap_t_scaled)
+
+            
+            
     
     plt.subplots(int(len(imWrap_s)/6)+1,6,figsize=(15,10))
     for ii in range(0,len(imWrap_t_wrap_t_s)):
